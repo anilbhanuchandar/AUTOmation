ab68a263caa976e51569557c1eca6416cf969524
AnilKumar <anilbhanuchandar@gmail.com>
2021-11-30 12:57:59 +0530

    First Round Of Commit...

---
+++ src/test/java/Maven/Automation.java
@@ -0,0 +1,19 @@
+package Maven;
+
+import org.testng.annotations.Test;
+
+public class Automation {
+	@Test
+	public void main()
+	{
+	   System.out.println("anil");
+		
+	}
+	@Test
+	public void main2()
+	{
+		System.out.println("nivce");
+	}
+	
+
+}
---
+++ src/test/java/Maven/RestAssured.java
@@ -0,0 +1,18 @@
+package Maven;
+
+import org.testng.annotations.Test;
+
+public class RestAssured {
+	@Test
+	public void clarity()
+	
+	{
+		System.out.println("clarity");
+	}
+	@Test
+	public void clarity2() {
+		System.out.println("clarity22");
+	}
+	
+
+}
